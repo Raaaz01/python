f24772c46ecb11b75f70af6536e6a0e78edd60ea
raju <rajkonduru01@gmail.com>
2022-09-12 16:43:52 +0530

    first commit

---
+++ 01.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+
+
+print ("hello")
+#print string
+print ("hello devops")
+#print number 
+print ("3")
+#print my name
+print ("raju")
---
+++ 02_string_variable.py
@@ -0,0 +1,10 @@
+print (20*30*24)
+#print (4*6)
+
+print (f"20 days are {20*24*60}minutes")
+
+#print (f"my name is {2*4}")
+#print (f"my age is {20-4}")
+
+#print (f"20 days are {20*24*60}")
+print (f"20 days are {20*24*60}minutes")
